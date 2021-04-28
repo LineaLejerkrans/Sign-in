901cef0f3010e90e6f7194f4057ab6359571bd3d
LineaLejerkrans <linlej020411@student.jenseneducation.se>
2021-04-28 16:14:55 +0200

    Add files via upload

--- LogIn/login.js
+++ LogIn/login.js
@@ -6,14 +6,16 @@ let pw1 = document.getElementById('pwd1')
  
  
 function store(){
+    if (localStorage.getItem(email2.value)){ // if the email you are trying to sign up with already exist in the storage 
+        alert("You're already logged in") // this will show and the rest of the function won't run
+        return;
+    }
     let person = { // store the email and the password in an object
         'email': email2.value,
         'password': pw2.value
     };
  
-    localStorage.setItem(email2.value, JSON.stringify(person)) // save the object with the email as its key, but you have to give the object the type of JSON string
- 
-    console.log(person)
+    localStorage.setItem(email2.value, JSON.stringify(person)) // save the object with the email as its key, and the value as an object but you have to give the object the type of a JSON string
 }
  
 function check(){
@@ -26,8 +28,6 @@ function check(){
         return; // if the email is wrong the function will end here 
     }
  
-    console.log(storeObj.email);
- 
     if(storeObj.password === pw1.value){ // if the email is correct you want to check the password 
         alert("You're logged in"); 
         return;
@@ -35,5 +35,3 @@ function check(){
  
     alert("Wrong password"); // if the email is right but the password is wrong this will be printed 
 }
- 
-
